280849f165315a9c2dd077f00006156d07b02766
gabriel-antonelli <gabrielantonelli25@gmail.com>
2022-07-20 16:18:22 -0300

    tests(createUser): add tests to use-case

---
+++ tests/core/use-case/create-user/createUser.spec.ts
@@ -0,0 +1,47 @@
+import { CreateUserImp } from '@/core/use-case/create-user';
+import { UserRepository, PasswordEncryptor } from '@/core/use-case/ports';
+import { mock } from 'jest-mock-extended';
+import { UserAlreadyCreatedError } from '@/core/use-case/create-user/errors/userAlreadyCreatedError';
+import { InvalidPasswordError } from '@/core/domain/user/errors';
+
+describe('Create User', () => {
+	const userRepositoryMock = mock<UserRepository>();
+	const passwordEncryptorMock = mock<PasswordEncryptor>();
+	const createUser = new CreateUserImp(
+		userRepositoryMock,
+		passwordEncryptorMock
+	);
+
+	test('Should create user', async () => {
+		userRepositoryMock.exists.mockResolvedValue(false);
+		passwordEncryptorMock.encrypt.mockResolvedValue('enctypted');
+		const createUserResponse = await createUser.createUser({
+			name: 'test',
+			email: 'test@test.com',
+			password: 'test1234@T',
+		});
+		expect(createUserResponse.value).toEqual(true);
+	});
+
+	test('Should throw user domain error(invalid password)', async () => {
+		const createUserResponse = await createUser.createUser({
+			name: 'test',
+			email: 'test@test.com',
+			password: 'test1234T',
+		});
+		expect(createUserResponse.value).toEqual(new InvalidPasswordError());
+	});
+
+	test('Should throw user already created error', async () => {
+		userRepositoryMock.exists.mockResolvedValue(true);
+		const alreadyCreatedEmail = 'test@test.com';
+		const createUserResponse = await createUser.createUser({
+			name: 'test',
+			email: alreadyCreatedEmail,
+			password: 'test1234@T',
+		});
+		expect(createUserResponse.value).toEqual(
+			new UserAlreadyCreatedError(alreadyCreatedEmail)
+		);
+	});
+});
