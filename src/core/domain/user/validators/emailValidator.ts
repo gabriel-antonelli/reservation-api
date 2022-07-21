c0bb9dfef6d1d4dcfef34ef8f45e10e3c951f07c
gabriel-antonelli <gabrielantonelli25@gmail.com>
2022-07-21 08:20:40 -0300

    hotfix: fix password and email regex, refactor router to use forEach instead of map

--- src/core/domain/user/validators/emailValidator.ts
+++ src/core/domain/user/validators/emailValidator.ts
@@ -1,6 +1,5 @@
 export const isEmailValid = (email: string): boolean => {
-	const tester =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const tester = /^\S+@\S+\.\S+$/;
 	if (!email || email.length > 255 || !tester.test(email)) {
 		return false;
 	}
--- src/core/domain/user/validators/passwordValidator.ts
+++ src/core/domain/user/validators/passwordValidator.ts
@@ -1,7 +1,6 @@
 export const isPasswordValid = (password: string): boolean => {
-	const passwordRegex = new RegExp(
-		'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})'
-	);
+	const passwordRegex =
+		/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])(?=.{8,})/;
 	if (!password || !passwordRegex.test(password)) {
 		return false;
 	}
