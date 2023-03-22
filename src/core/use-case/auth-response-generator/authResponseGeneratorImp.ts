e563486f31e975c8b002ca68456e5b3e59d64a54
gabriel-antonelli <gabrielantonelli25@gmail.com>
2023-03-22 07:26:47 -0300

    fix: added constants to authResponseGeneratorImp

--- src/core/use-case/auth-response-generator/authResponseGeneratorImp.ts
+++ src/core/use-case/auth-response-generator/authResponseGeneratorImp.ts
@@ -3,11 +3,17 @@ import { JWT } from '../ports';
 import { AuthResponseGenerator } from './authResponseGenerator';
 
 export class AuthResponseGeneratorImp implements AuthResponseGenerator {
+	private readonly TOKEN_EXPIRATION = '5m';
+	private readonly REFRESH_TOKEN_EXPIRATION = '8m';
+
 	constructor(private readonly jwtSigner: JWT) {}
 
 	async generateAuthResponse(email: string): Promise<false | AuthResponse> {
-		const token = await this.jwtSigner.sign(email, '5m');
-		const refreshToken = await this.jwtSigner.sign(email, '8m');
+		const token = await this.jwtSigner.sign(email, this.TOKEN_EXPIRATION);
+		const refreshToken = await this.jwtSigner.sign(
+			email,
+			this.REFRESH_TOKEN_EXPIRATION
+		);
 
 		if (!token || !refreshToken) {
 			return false;
